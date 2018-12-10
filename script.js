5ca078a18be22f1d8593745931b3fcbeb7b2026c
Louiseappleton <louisedfappleton@gmail.com>
2018-12-10 20:20:38 +0000

    Fixed a bug and improved the carousel

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var myIndex = 0;
-carousel();
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+setInterval(carousel, 2000);
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
+    for (var i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
     }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    slideIndex++;
+    if (slideIndex >= slides.length) {slideIndex = 0}
+    slides[slideIndex].style.display = "block";
 }
